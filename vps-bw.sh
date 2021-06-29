3f7da55825e0c2ed2b3856c3d93be07f5ee31e26
hzzz <hzzz0823@gmail.com>
2021-06-29 14:34:31 +0800

    vps print format

--- vps-bw.sh
+++ vps-bw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 a=`curl -s "https://justmysocks3.net/members/getbwcounter.php?service=367706&id=3b7af7bb-86a3-4e64-8f20-f2f14d29e658" | jq .bw_counter_b`
-used=`bc <<< "scale=2; $a/1024/1024/1024"`
-echo $[a/1024/1024/1024/5]% ${used}G
+used=`bc <<< "scale=2; $a/1024/1024/1024" | awk '{printf "%.2f", $0}'`
+percent=`bc <<< "scale=2; $used/5" | awk '{printf "%.2f", $0}'`
+echo ${percent}%: ${used} GB used out of 500 GB
+
