1fd7745293ad8972f3f3b74394948c9ac634da15
hzzz <hzzz0823@gmail.com>
2021-07-19 11:59:58 +0800

    update vps api

--- vps-bw.sh
+++ vps-bw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-url="https://justmysocks3.net/members/getbwcounter.php?service=367706&id=3b7af7bb-86a3-4e64-8f20-f2f14d29e658"
+url="https://justmysocks3.net/members/getbwcounter.php?service=367706&id=4c24d318-ece5-4879-899b-fd4c2bb8c9de"
 a=`curl -s ${url} | jq .bw_counter_b`
 used=`bc <<< "scale=2; $a/1024/1024/1024" | awk '{printf "%.2f", $0}'`
 percent=`bc <<< "scale=2; $used/5" | awk '{printf "%.2f", $0}'`
