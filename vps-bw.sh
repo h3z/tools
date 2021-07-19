3dc3f853c3682a4c9fd104b129122fd122059426
hzzz <hzzz0823@gmail.com>
2021-07-19 11:56:51 +0800

    update vps api

--- vps-bw.sh
+++ vps-bw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-a=`curl -s "https://justmysocks3.net/members/getbwcounter.php?service=367706&id=3b7af7bb-86a3-4e64-8f20-f2f14d29e658" | jq .bw_counter_b`
+url="https://justmysocks3.net/members/getbwcounter.php?service=367706&id=3b7af7bb-86a3-4e64-8f20-f2f14d29e658"
+a=`curl -s ${url} | jq .bw_counter_b`
 used=`bc <<< "scale=2; $a/1024/1024/1024" | awk '{printf "%.2f", $0}'`
 percent=`bc <<< "scale=2; $used/5" | awk '{printf "%.2f", $0}'`
 echo ${percent}%: ${used} GB used out of 500 GB
