6bd4187d0eebeba9b811e5fe23c34083563cc537
hzzz <hzzz@hzzzdeMacBook-Pro.local>
2021-08-23 09:30:03 +0800

    ..

--- vps-bw.sh
+++ vps-bw.sh
@@ -2,6 +2,6 @@
 url="https://justmysocks3.net/members/getbwcounter.php?service=367706&id=4c24d318-ece5-4879-899b-fd4c2bb8c9de"
 a=`curl -s ${url} | jq .bw_counter_b`
 used=`bc <<< "scale=2; $a/1024/1024/1024" | awk '{printf "%.2f", $0}'`
-percent=`bc <<< "scale=2; $used/5" | awk '{printf "%.2f", $0}'`
-echo ${percent}%: ${used} GB used out of 500 GB
+percent=`bc <<< "scale=2; $used/10" | awk '{printf "%.2f", $0}'`
+echo ${percent}%: ${used} GB used out of 1 TB
 
