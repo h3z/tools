bbd4f7a07671e06b3605e7c56a3b0131120019b8
hzzz <you@example.com>
2022-01-24 11:25:42 +0800

    .

--- vps-bw.sh
+++ vps-bw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-url="https://justmysocks3.net/members/getbwcounter.php?service=367706&id=4c24d318-ece5-4879-899b-fd4c2bb8c9de"
+#url="https://justmysocks3.net/members/getbwcounter.php?service=367706&id=4c24d318-ece5-4879-899b-fd4c2bb8c9de"
+url="https://justmysocks5.net/members/getbwcounter.php?service=367706&id=4c24d318-ece5-4879-899b-fd4c2bb8c9de"
 a=`curl -s ${url} | jq .bw_counter_b`
 used=`bc <<< "scale=2; $a/1024/1024/1024" | awk '{printf "%.2f", $0}'`
 percent=`bc <<< "scale=2; $used/10" | awk '{printf "%.2f", $0}'`
